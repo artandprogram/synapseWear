b4f8f04d4e0600a44d249f21e6a6ccb6f78ce87c
Alexander Reeder <alexr@amber-4.local>
2018-10-04 15:37:38 +0900

    Filling out LPS25H support

--- firmware/Arduino/synapseWear/LPS25H.cpp
+++ firmware/Arduino/synapseWear/LPS25H.cpp
@@ -3,22 +3,16 @@
 #include "LPS25H.h"
 #include "Wire.h"
 
-// TODO
-// use I2C
-// change for intervals
-// sleep
-
 bool LPS25H::begin()
 {
-  isConnected = true;
+  int result = I2Cdev::readByte(LPS25H_ADDRESS, LPS25H_WHO_AM_I, buf);
 
-  Wire.beginTransmission(LPS25H_ADDRESS);
-  // Select control register 1
-  Wire.write(0x20);
-  // Set active mode, continuous Update
-  Wire.write(0x90);
-  // Stop I2C Transmission
-  Wire.endTransmission();
+  if (buf[0] != 0xBD) {
+    Serial.println("Can't access LPS25H");
+    return false;
+  }
+
+  isConnected = true;
 
   return true;
 }
@@ -28,17 +22,14 @@ bool LPS25H::enable()
   if (isEnabled)
     return true;
 
-  //writeRegister(MODE_REGISTER, 0x20, 0x00);
+  I2Cdev::writeByte(LPS25H_ADDRESS, LPS25H_CTRL_REG1, LPS25H_POWERON7HZ);
 
   isEnabled = true;
 }
 
 bool LPS25H::disable()
 {
-//  writeRegister(MODE_REGISTER, 0x20, 0x01);
-//  for (int i = 0; i < 4; i++) {
-//    buf[i] = 0xff;
-//  }
+  I2Cdev::writeByte(LPS25H_ADDRESS, LPS25H_CTRL_REG1, LPS25H_POWERDOWN);
 
   isEnabled = false;
 }
@@ -48,35 +39,7 @@ bool LPS25H::update()
   if (!isEnabled)
     return false;
 
-  unsigned int data[3];
-  // Start I2C Transmission
-  Wire.beginTransmission(LPS25H_ADDRESS);
-  // Select pressure data register
-  Wire.write(0x28 | 0x80);
-  // Stop I2C Transmission
-  Wire.endTransmission();
-
-  // Request 3 bytes of data
-  Wire.requestFrom(LPS25H_ADDRESS, 3);
-
-  // Read 3 bytes of data
-  // pressure lsb first
-  if (Wire.available() == 3)
-  {
-    data[0] = Wire.read();
-    data[1] = Wire.read();
-    data[2] = Wire.read();
-  }
-
-  // Convert pressure data
-  pressure = ((data[2] * 65536) + (data[1] * 256) + data[0]) / 4096.0;
-
-  // Output data to serial monitor
-//  Serial.print("Pressure is : ");
-//  Serial.print(pressure);
-//  Serial.println(" hPa");
-  
-//  voltage = getVCell();
-//  stateOfCharge = getSoC();
+  I2Cdev::readBytes(LPS25H_ADDRESS, 0x28 | 0x80, 3, buf);
+  pressure = ((buf[2] * 65536) + (buf[1] * 256) + buf[0]) / 4096.0;
 }
 
--- firmware/Arduino/synapseWear/LPS25H.h
+++ firmware/Arduino/synapseWear/LPS25H.h
@@ -1,11 +1,17 @@
 // LPS25H library for synapseWear
 
 #include "Arduino.h"
+#include "I2Cdev.h"
 
 #ifndef _LPS25H_H
 #define _LPS25H_H
 
-#define LPS25H_ADDRESS	0x5C
+#define LPS25H_ADDRESS	      0x5C
+#define LPS25H_WHO_AM_I       0x0F
+#define LPS25H_CTRL_REG1      0x20
+#define LPS25H_POWERON1HZ     0x90
+#define LPS25H_POWERON7HZ     0xA0
+#define LPS25H_POWERDOWN      0x0
 
 class LPS25H {
 
@@ -19,7 +25,7 @@ class LPS25H {
     bool isConnected = false;
     bool isEnabled = false;
     float pressure = 0.0f;
-
+    uint8_t buf[3] = {0};
 };
 
 #endif
